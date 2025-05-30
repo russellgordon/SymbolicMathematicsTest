b931dd8555a8f1fbd9efe92fc1802b402f6bf7b4
Russell Gordon <rgordon@lcs.on.ca>
2025-05-30 08:20:20 -0400

    Initial Commit

---
+++ SymbolicMathematicsTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SymbolicMathematicsTest
+//
+//  Created by Russell Gordon on 2025-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SymbolicMathematicsTest/SymbolicMathematicsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolicMathematicsTestApp.swift
+//  SymbolicMathematicsTest
+//
+//  Created by Russell Gordon on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct SymbolicMathematicsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
